cd20237479e5f45baf5c16d7818cf77d36205f65
Richard Taylor <richard.t@fullmetrics.com>
2026-05-20 14:56:47 +0100

    Update taykit to 1.3.3

--- Formula/taykit.rb
+++ Formula/taykit.rb
@@ -1,9 +1,9 @@
 class Taykit < Formula
   desc "Taylor bioinformatics command-line toolkit"
   homepage "https://github.com/richardmarktaylor/taykit"
-  url "https://github.com/richardmarktaylor/taykit/releases/download/v1.3.2/taykit-macos-arm64.tar.gz"
-  sha256 "b1844f4eb7278ec4fc9802d758e34833a3b17940ad4021d10a50dd25d0044926"
-  version "1.3.2"
+  url "https://github.com/richardmarktaylor/taykit/releases/download/v1.3.3/taykit-macos-arm64.tar.gz"
+  sha256 "699cf874656f3c045ee2ff68e8665ccab82e2ef98eb374e5b19522968706393f"
+  version "1.3.3"
 
   def install
     bin.install "taykit"
