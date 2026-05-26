502e705cd7400c232ee16f36f2ae099fa46a6d27
Richard Taylor <richard.t@fullmetrics.com>
2026-05-26 20:05:31 +0100

    Update taykit to 1.5.5

--- Formula/taykit.rb
+++ Formula/taykit.rb
@@ -1,9 +1,9 @@
 class Taykit < Formula
   desc "Taylor bioinformatics command-line toolkit"
   homepage "https://github.com/richardmarktaylor/taykit"
-  url "https://github.com/richardmarktaylor/taykit/releases/download/v1.5.4/taykit-macos-arm64.tar.gz"
-  sha256 "c75402022b302ff0c6a3079c38f75ff58ec34b9479fb613eb73e803947abe841"
-  version "1.5.4"
+  url "https://github.com/richardmarktaylor/taykit/releases/download/v1.5.5/taykit-macos-arm64.tar.gz"
+  sha256 "c2a29cea8dc4b60636954bf3d9fa9b5922d1a651ba85e0b684d1b76951a9d507"
+  version "1.5.5"
 
   def install
     bin.install "taykit"
